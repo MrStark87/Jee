829b4fc9a1ea55c373c4415864996b1598779b0d
MrStark87 <mauryaanshmaurya2008@gmail.com>
2026-04-07 07:41:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,8 +4,8 @@ Edit BOT_TOKEN and ADMIN_ID before running.
 """
 
 BOT_TOKEN = "YOUR_BOT_TOKEN_HERE"   # BotFather se liya token
-ADMIN_ID   = 123456789               # Apna Telegram user ID (int)
-ADMIN_PASS = "yourpassword"          # Admin panel password
+ADMIN_ID   = 8275132868              # Apna Telegram user ID (int)
+ADMIN_PASS = "anya"          # Admin panel password
 
 DB_PATH    = "jee_saarthi.db"
 TIMEZONE   = "Asia/Kolkata"
